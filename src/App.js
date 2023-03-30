8fe8c6803c5e2d842f0ace25662b1de059e7ad22
razal <oungaounga@gmail.com>
2023-03-30 09:41:44 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import {Fragment} from 'react'
+
+import './App.css'
 
 function App() {
+  console.log('hello from App JS')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>hello from firebase test</h3>
+    </>
+  )
 }
 
-export default App;
+export default App
