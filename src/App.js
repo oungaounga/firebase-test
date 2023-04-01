73831d19a09a92ca8fc69ba210bc874650cb3a55
razal <oungaounga@gmail.com>
2023-04-01 16:25:39 +0200

    just playing with data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import './Global.css'
+import {FirebaseApp} from 'firebase/app'
+import {initializeApp} from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  addDoc,
+  deleteDoc,
+} from 'firebase/firestore/lite'
+import {useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDDSPSpUYtkSOdbOg5Yyuoc5VME_gjv5Q',
+  authDomain: 'fire-test-3c6f5.firebaseapp.com',
+  projectId: 'fire-test-3c6f5',
+  storageBucket: 'fire-test-3c6f5.appspot.com',
+  messagingSenderId: '878068415264',
+  appId: '1:878068415264:web:81faae9b0a10b7c484f545',
+}
+console.clear()
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const usersCollection = collection(db, 'users')
+const docRef = doc(db, 'users', '8blZVi95oK8CceTwsYi3')
+
+const userQuentin = {
+  name: 'quentin',
+  email: 'quentin@example.com',
+  tel: '777-777-777',
+}
+const userIris = {
+  name: 'iris',
+  email: 'iris@example.com',
+  tel: '777-777-777',
 }
 
-export default App;
+const userJeev = {
+  name: 'jeev',
+  email: 'jeev@example.com',
+  tel: '777-777-777',
+}
+
+function addDataToCollection(collection, data) {
+  addDoc(collection, data)
+    .then(() => {
+      console.log('data added')
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+}
+
+function addDocToCollection(collection, docName, doc) {
+  setDoc(doc(collection, docName), doc)
+    .then(() => {
+      console.log(`${docName} added`)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+}
+
+//TODOs :
+// make functions that push pull data from firestore
+// function will be called when page's loaded
+// no point of making it work before components mounted
+// using forms
+
+//the adding or ommiting data from database shouldnt be
+// in hooks, runs twice, only if manage to exec hooks
+// once
+
+//Database tools to master :
+// - Adding data
+// - Deleting data
+// - Updating : Careful, data can be overwritten, uses careful IDs
+// - Creating collections
+// - Query Data, i.e. all the query methods in firestore
+// - compute on data maybe
+// - Render elements from data : 1. Data must be loaded to variables
+//                                before components mounts
+//                                Feed components the data
+//                                 we good
+//
+
+function App() {
+  // useEffect(() => {
+  // }, [])
+
+  getDoc(docRef).then((result) => {
+    console.log('no use effect', result.data())
+  })
+  setDoc(doc(collection(db, 'users'), 'Molly'), {
+    email: 'molly@example.com',
+    name: 'new molly',
+    tel: '777-777-777',
+    overwritten: true,
+  })
+    .then(() => {
+      console.log('data added')
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+
+  return (
+    <div>
+      <h3>salut salut</h3>
+    </div>
+  )
+}
+
+export default App
---
+++ src/fbConfig.js
@@ -0,0 +1,21 @@
+/** @format */
+
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDDSPSpUYtkSOdbOg5Yyuoc5VME_gjv5Q',
+  authDomain: 'fire-test-3c6f5.firebaseapp.com',
+  projectId: 'fire-test-3c6f5',
+  storageBucket: 'fire-test-3c6f5.appspot.com',
+  messagingSenderId: '878068415264',
+  appId: '1:878068415264:web:81faae9b0a10b7c484f545',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
